f4c19185ce831226f826bcc21f7d903583d648af
Vivek-Malla <thisisvivekmalla@gmail.com>
2025-07-16 14:48:12 -0400

    solution

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,1 +1,20 @@
-# Array
+def two_Sum(arr, target):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i] + arr[j] == target:
+                return [i,j]
+
+n = int(input("Enter a count:"))
+
+arr = []
+
+for i in range(n):
+    num = int(input(""))
+    arr.append(num)
+
+target = int(input("enter target :"))
+
+print(two_Sum(arr,target))
+
+
+
